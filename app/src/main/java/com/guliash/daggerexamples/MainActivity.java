0ea8e8a63f9afca884570681e340bd58e4830e38
Artem Gilmudinov <guliash92@gmail.com>
2020-04-09 17:45:33 +0300

    testing inject;

--- app/src/main/java/com/guliash/daggerexamples/MainActivity.java
+++ app/src/main/java/com/guliash/daggerexamples/MainActivity.java
@@ -3,8 +3,11 @@ package com.guliash.daggerexamples;
 import android.app.Activity;
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 import dagger.Binds;
 import dagger.Module;
+import dagger.Provides;
 import dagger.Subcomponent;
 import dagger.android.AndroidInjection;
 import dagger.android.AndroidInjector;
@@ -13,19 +16,32 @@ import dagger.multibindings.IntoMap;
 
 public class MainActivity extends Activity {
 
+    @Inject
+    Integer integer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         AndroidInjection.inject(this);
+
+        System.out.println("Log " + integer);
     }
 }
 
-@Subcomponent
+@Subcomponent(modules = ActivityModule.class)
 interface ActivitySubcomponent extends AndroidInjector<MainActivity> {
     @Subcomponent.Factory
     public interface Factory extends AndroidInjector.Factory<MainActivity> {}
+}
+
+@Module
+class ActivityModule {
+    @Provides
+    public Integer provideInteger() {
+        return 42;
+    }
 }
 
 @Module(subcomponents = ActivitySubcomponent.class)
