12cc75b5214d8c65c763bfaaa2003353fa4c61c6
Artem Gilmudinov <guliash92@gmail.com>
2020-04-09 16:32:09 +0300

    up and fix build;

--- app/src/main/java/com/guliash/daggerexamples/ComponentA.java
+++ app/src/main/java/com/guliash/daggerexamples/ComponentA.java
@@ -2,9 +2,7 @@ package com.guliash.daggerexamples;
 
 import dagger.Component;
 
-@Component(modules = ModuleA.class)
+@Component
 public interface ComponentA {
     void inject(MainActivity activity);
-
-    ComponentB plus();
 }
--- app/src/main/java/com/guliash/daggerexamples/MainActivity.java
+++ app/src/main/java/com/guliash/daggerexamples/MainActivity.java
@@ -7,16 +7,11 @@ import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Inject
-    String string;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DaggerComponentA.create().plus().inject(this);
-
-        System.out.println(string); //A
+        DaggerComponentA.create().inject(this);
     }
 }
