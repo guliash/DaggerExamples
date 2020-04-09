f995b9a21029a05c059ea0b4553e4b8fe72c60e8
Artem Gilmudinov <guliash92@gmail.com>
2020-04-09 16:58:26 +0300

    wip;

--- app/src/main/java/com/guliash/daggerexamples/MainActivity.java
+++ app/src/main/java/com/guliash/daggerexamples/MainActivity.java
@@ -3,11 +3,35 @@ package com.guliash.daggerexamples;
 import android.app.Activity;
 import android.os.Bundle;
 
+import dagger.Binds;
+import dagger.Module;
+import dagger.Subcomponent;
+import dagger.android.AndroidInjection;
+import dagger.android.AndroidInjector;
+import dagger.multibindings.ClassKey;
+import dagger.multibindings.IntoMap;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AndroidInjection.inject(this);
     }
+}
+
+@Subcomponent
+interface ActivitySubcomponent extends AndroidInjector<MainActivity> {
+    @Subcomponent.Factory
+    public interface Factory extends AndroidInjector.Factory<MainActivity> {}
+}
+
+@Module(subcomponents = ActivitySubcomponent.class)
+abstract class ActivityLinkingModule {
+    @Binds
+    @IntoMap
+    @ClassKey(MainActivity.class)
+    abstract AndroidInjector.Factory<?> bindYourAndroidInjectorFactory(ActivitySubcomponent.Factory factory);
 }
--- app/src/main/java/com/guliash/daggerexamples/MainApplication.java
+++ app/src/main/java/com/guliash/daggerexamples/MainApplication.java
@@ -2,5 +2,32 @@ package com.guliash.daggerexamples;
 
 import android.app.Application;
 
-public class MainApplication extends Application {
+import javax.inject.Inject;
+
+import dagger.Component;
+import dagger.android.AndroidInjectionModule;
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasAndroidInjector;
+
+public class MainApplication extends Application implements HasAndroidInjector {
+    @Inject
+    DispatchingAndroidInjector<Object> dispatchingAndroidInjector;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DaggerApplicationComponent.create().inject(this);
+    }
+
+    @Override
+    public AndroidInjector<Object> androidInjector() {
+        return dispatchingAndroidInjector;
+    }
+}
+
+
+@Component(modules = { AndroidInjectionModule.class, ActivityLinkingModule.class })
+interface ApplicationComponent {
+    void inject(MainApplication application);
 }
